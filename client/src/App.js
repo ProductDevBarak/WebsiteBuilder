1bc2c6ef4a76d270d3b9d1be84dcb153a21f68ae
aditya-samal <samaladitya2004@gmail.com>
2025-01-16 00:59:21 +0530

    Auth

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,23 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
 function App() {
-  return <div>Hi</div>;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        {/* Add more routes as needed */}
+      </Routes>
+    </BrowserRouter>
+  );
+}
+
+function Home() {
+  return <h1>Welcome to the Home Page</h1>;
+}
+
+function About() {
+  return <h1>About Us</h1>;
 }
 
 export default App;
---
+++ server/index.js
@@ -0,0 +1,31 @@
+import express from "express";
+import dotenv from "dotenv";
+import mongoose from "mongoose";
+import cors from "cors";
+// import AuthRoute from "./routes/Auth.route.js";
+import cookieParser from "cookie-parser";
+
+dotenv.config();
+
+const PORT = process.env.PORT;
+const app = express();
+
+app.use(express.json());
+app.use(cookieParser());
+app.use(
+  cors({
+    origin: "http://localhost:5173",
+    credentials: true,
+  })
+);
+
+// app.use("/api/auth", AuthRoute);
+
+mongoose
+  .connect(process.env.DB_URL)
+  .then(() => console.log("Database connected."))
+  .catch((err) => console.log("Database connection failed.", err));
+
+app.listen(PORT, () => {
+  console.log("Server running on port:", PORT);
+});
